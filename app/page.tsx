4a40e2f13c314dae7a67d8c97ea4137de17cd05e
yurii_fishbakh <bunbanto@gmail.com>
2025-06-19 22:15:01 +0200

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
       <p className={css.description}>
         NoteHub is a simple and efficient application designed for managing
         personal notes. It helps keep your thoughts organized and accessible in
-        one place, whether you're at home or on the go.
+        one place, whether you`re at home or on the go.
       </p>
       <p className={css.description}>
         The app provides a clean interface for writing, editing, and browsing
